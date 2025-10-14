2b88a1f4fb56f8b44306694f54c3ebf5d8bd91ae
yadhav5 <yadhavmkd5@gmail.com>
2025-10-14 11:22:34 +0530

    Add files via upload

---
+++ roman_int.py
@@ -0,0 +1,19 @@
+
+def roman_to_int(s):
+    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+
+    for char in reversed(s):
+        value = roman_values[char]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+
+    return total
+
+roman = input("Enter Roman Number: ").upper()
+print("Integer value:", roman_to_int(roman))
